6951b9f7db05aaba83a6089c449f802f7ada81ed
Andrey Zaytsev <and.zaytsev9@gmail.com>
2020-12-21 15:05:06 +0300

    New structure, little body

--- main.py
+++ main.py
@@ -1,32 +1,48 @@
 import json
 
-with open('newsafr.json',encoding='utf-8') as f:
-    json_data = json.load(f)
+def read_file(file):
+    with open(file, encoding='utf-8') as f:
+        json_data = json.load(f)
+        return json_data
 
-all_txt = []
-for news in json_data['rss']['channel']['items']:
-    description = news['description'].split(' ')
-    all_txt += description
+def get_txt(file):
+    all_txt = []
+    data = read_file(file)
+    for news in data['rss']['channel']['items']:
+        description = news['description'].split(' ')
+        all_txt += description
+    return all_txt
 
-long_words = []
-for word in all_txt:
-    # print(word)
-    if len(word) > 6:
-        long_words.append(word)
+def get_long_words(file):
+    words_list = []
+    txt = get_txt(file)
+    for word in txt:
+        if len(word) > 6:
+            words_list.append(word)
+    return words_list
 
-set_long_words = set(long_words)
+def get_repeats_of_words(file):
+    unique_words = {}
+    long_words = get_long_words(file)
+    for word in long_words:
+        if word not in unique_words:
+            unique_words[word] = 1
+        else:
+            unique_words[word] += 1
+    return unique_words
 
-list = []
-top = []
+def get_sorted_top(file):
+    repeats = get_repeats_of_words(file)
+    tuples_list = list(repeats.items())
+    tuples_list.sort(key=lambda file: file[1], reverse=True)
+    top = []
+    for word in tuples_list[:10]:
+        top.append(word[0])
+    print(tuples_list)
+    return top
 
-for word in set_long_words:
-    number_of_repeats = long_words.count(word)
-    list.append([word, number_of_repeats])
 
-list.sort(key = lambda file: file[1], reverse=True)
-for word in list[:10]:
-    top.append(word[0])
-
+top = get_sorted_top('newsafr.json')
 print(top)
 
 
